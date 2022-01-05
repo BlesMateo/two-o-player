7cb3b56da1039b623fac1fbf97f0e329da9591be
BlesMateo <blesildaangelicamateo@gmail.com>
2022-01-05 01:17:57 -0500

    Defined game start if both players are still playing

--- game.rb
+++ game.rb
@@ -5,4 +5,7 @@ class Game
     @current_player = @player1
     @player1 = Player.new('Player 1')
     @player2 = Player.new('Player 2')
+
+    @turn = {:player => @player1, :correct? => false}
+    
   end
